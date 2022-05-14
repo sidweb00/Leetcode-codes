06cd76b301c044323d68e47d973de83edce80871
Siddharth Gandhi <66740260+sidweb00@users.noreply.github.com>
2022-05-15 01:13:01 +0530

    Time: 17 ms (73.81%), Space: 10.5 MB (66.93%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     
-    int lower_bound(vector<int>a,int t){
-        int l=0,r=a.size();
-        int ans=0;
-        while(l<=r){
-            int mid=(l+r)/2;
-            
-            if(a[mid]>=t){
-                ans=mid;
-                r=mid-1;
-            }
-            else l=mid+1;
-        }
-        return ans;
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         vector<int>sols;
         
@@ -24,8 +9,8 @@ public:
                 sols.push_back(v);
             }
             else{
-                int i=lower_bound(sols,v);
-                sols[i]=v;
+                auto it=std::lower_bound(sols.begin(),sols.end(),v);
+                *it=v;
             }
         }
         return sols.size();
