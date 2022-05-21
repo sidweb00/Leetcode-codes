fd2d5452b0e81eb10ecc5e1389888c892ed9b3e8
Siddharth Gandhi <66740260+sidweb00@users.noreply.github.com>
2022-05-21 16:07:34 +0530

    Time: 4 ms (22.44%), Space: 5.7 MB (97.95%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     
-    int catalan(int n,vector<int>&dp){
-        if(dp[n])return dp[n];
+    int dp[20];
+    
+    int rec(int x){
+        if(x<=1)return 1;
+        
+        if(dp[x])return dp[x];
         int ans =0;
-        for(int i=0;i<n;i++){
-            ans+=catalan(i,dp)*catalan(n-i-1,dp);
+        for(int i=0;i<x;i++){
+            ans+=rec(i)*rec(x-i-1);
         }
-       dp[n] = ans; 
-        return ans;
+        return dp[x]=ans;
     }
     
+    
     int numTrees(int n) {
-        vector<int>dp(n+1);
-        dp[0]=dp[1]=1;
-        return catalan(n,dp);
+        return rec(n);
     }
 };
