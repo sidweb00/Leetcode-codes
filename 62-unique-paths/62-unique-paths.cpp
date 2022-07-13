a110951dbca61877d348f60906f8c0ab4c668e8b
Siddharth Gandhi <66740260+sidweb00@users.noreply.github.com>
2022-07-13 19:41:29 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    
-    int dp[110][110];
-    int M,N;
-    int rec(int i,int j){
-        if(i==M-1 && j==N-1){
-            return 1;
-        }
-        
-        if(i>=M || j>=N)return 0;
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        dp[i][j]=rec(i+1,j)+rec(i,j+1);
-        return dp[i][j];
-    }
-    
     int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
-        M=m;N=n;
-        return rec(0,0);
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
