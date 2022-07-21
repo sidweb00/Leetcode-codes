12ec23bf5d00e30e4c572d6457900edbf31a5e8e
Siddharth Gandhi <66740260+sidweb00@users.noreply.github.com>
2022-07-22 00:06:56 +0530

    Time: 22 ms (53.24%), Space: 13.3 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        
         int n=matrix.size();
-    int m=matrix[0].size();
-    
-    set<int>row,col;
-    
-    for(int i=0;i<n;i++){
+        int m=matrix[0].size();
+        
+        bool col=0,row=0;
+        
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0)col=1;
+        }
+        
         for(int j=0;j<m;j++){
-            if(matrix[i][j]==0){
-                row.insert(i);
-                col.insert(j);
+            if(matrix[0][j]==0)row=1;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
             }
         }
-    }
-    
-    for(int i=0;i<n;i++){
-        if(row.find(i)!=row.end()){
-            for(int j=0;j<m;j++){
-                matrix[i][j]=0;
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
             }
         }
-    }
-    
-    for(int j=0;j<m;j++){
-        if(col.find(j)!=col.end()){
-            for(int i=0;i<n;i++){
-                matrix[i][j]=0;
-            }
+        
+        if(col){
+            for(int i=0;i<n;i++)matrix[i][0]=0;
         }
-    }
+        
+        if(row){
+            for(int j=0;j<m;j++)matrix[0][j]=0;
+        }
     }
 };
