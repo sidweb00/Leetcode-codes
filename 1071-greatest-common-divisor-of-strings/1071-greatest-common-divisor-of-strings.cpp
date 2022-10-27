de8b929de2688dff2e925f2fe308d20953d0c5f8
Siddharth Gandhi <66740260+sidweb00@users.noreply.github.com>
2022-10-27 20:29:44 +0530

    Time: 10 ms (31.39%), Space: 7 MB (77.77%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     string gcdOfStrings(string s1, string s2) {
         
-        return (s1+s2 ==s2+s1)?s1.substr(0,gcd(s1.length(),s2.length())):"";
+        return (s1+s2==s2+s1)?s1.substr(0,gcd(s1.length(),s2.length())):"";
     }
 };
